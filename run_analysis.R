a9d17e534d5a9628092606f597919fc298342118
John Hurley <jhurley@mac.com>
2018-02-09 00:10:16 -0800

    add reshape2

--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,7 @@
 #       https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 library(dplyr)
+library(reshape2)
 
 # ------------------------------------------------------------------------
 # You should create one R script called run_analysis.R that does the following. 
